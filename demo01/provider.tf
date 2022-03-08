109c85cc53fcfd5cbdb0f77cf5b1ed669a4fb05f
Alejandro de Alda <adealdag@cisco.com>
2022-03-08 11:08:39 +0100

    Merge provider config files

--- demo01/provider.tf
+++ demo01/provider.tf
@@ -15,3 +15,18 @@ provider "mso" {
   platform = "nd"
   domain   = "dcmdr1"
 }
+
+# Cisco MSO provider variables
+variable "mso_url" {
+  description = "URL for CISCO MSO/NDO"
+}
+
+variable "mso_username" {
+  description = "This is the Cisco MSO/NDO username, which is required to authenticate with CISCO MSO/NDO"
+  default     = "terraform"
+}
+
+variable "mso_password" {
+  description = "Password of the user mentioned in username argument."
+  sensitive   = true
+}
